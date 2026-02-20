9aee602619776503c47f1ceff77040d7df529c73
Periyaun <periyaung123@gmail.com>
2026-02-20 18:44:27 -0500

    Functions created to hash and verify passwords

---
+++ workout-tracker-backend/app/auth.py
@@ -0,0 +1,31 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from typing import Optional
+
+# pass hash config
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# jwt config 
+SECRET_KEY = "your-secret-key-here-change-this-in-production"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # checks if the password matches the hashed one
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password(password: str) -> str:
+    # hashes a password
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    # create jwt token
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
