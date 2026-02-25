66bd9fd5d8443754454b8a74b88096f51fd253d1
Periyaun <periyaung123@gmail.com>
2026-02-25 15:14:50 -0500

    A form with username and password inputs that calls the api, stores token, and redirects

--- workout-tracker-frontend/src/pages/Login.jsx
+++ workout-tracker-frontend/src/pages/Login.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { loginUser } from "../api/auth"
+import useAuthStore from "../store/authStore"
+
 function Login() {
-    return <h1>Login</h1>
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+
+    const login = useAuthStore((state) => state.login)
+    const navigate = useNavigate()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        setError(null)
+
+        try {
+            const data = await loginUser(username, passsword)
+            login(data.access_token)
+            navigate("/")
+        }
+        catch (err) {
+            setError("Invalid username or password")
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                {error && <p>{error}</p>}
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login
