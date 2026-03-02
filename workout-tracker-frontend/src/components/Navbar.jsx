5b56cc33c0af91987381f35ae49adbe1f36d5d3c
Periyaun <periyaung123@gmail.com>
2026-03-02 13:34:23 -0500

    Updating the navbar to allow logouts

--- workout-tracker-frontend/src/components/Navbar.jsx
+++ workout-tracker-frontend/src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"
+import useAuthStore from "../store/authStore"
 
 function Navbar() {
+    const token = useAuthStore((state) => state.token)
+    const logout = useAuthStore((state) => state.logout)
+    const navigate = useNavigate()
+
+    function handleLogout() {
+        logout()
+        navigate("/login")
+    }
+
     return (
-    <nav>
-    <Link to="/">Dashboard</Link>
-    <Link to="/log">Log Workout</Link>
-    <Link to="/login">Login</Link>
-    </nav>
-  )
+        <nav>
+            <Link to="/">Dashboard</Link>
+            <Link to="/log">Log Workout</Link>
+            {token ? (
+                <button onClick={handleLogout}>Logout</button>
+            ) : (
+                <Link to="/login">Login</Link>
+            )}
+        </nav>
+    )
 }
 
 export default Navbar
